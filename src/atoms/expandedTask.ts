628539389462831079b7611b702d7d6b1059c324
Travis Tang <yiu.tang@c02d80u3md6v.check24.intern>
2022-12-16 11:36:38 +0100

    TWO-13: Remove toggle expand all logic

--- src/atoms/expandedTask.ts
+++ src/atoms/expandedTask.ts
@@ -1,6 +1,5 @@
-import { atom, useRecoilState, useRecoilValue } from "recoil";
+import { atom, useRecoilState } from "recoil";
 import { addWithoutRepeat, toggleElement } from "../utils/array";
-import taskAtom from "./tasks";
 
 export const expandedTaskAtom = atom<string[]>({
   key: "expanded-task",
@@ -20,38 +19,5 @@ export const useExpandTask = (taskId: string, forceState?: boolean) => {
       return;
     }
     setExpandedTaskAtom(toggleElement(expandedTasksId, taskId));
-  };
-};
-export const useToggleExpandAll = (toggleTaskId: string) => {
-  const tasksMap = useRecoilValue(taskAtom);
-  const [expandedTasksId, setExpandedTaskAtom] =
-    useRecoilState(expandedTaskAtom);
-  const tasks = Object.values(tasksMap);
-  const getSubTasks = (taskId: string): string[] => {
-    const subTasks = tasks
-      .filter((task) => task.parentId === taskId)
-      .map((task) => task.id);
-    return [taskId, ...subTasks.flatMap(getSubTasks)];
-  };
-
-  const allTasks = getSubTasks(toggleTaskId);
-  const allExpanded = allTasks.every((subTaskId) =>
-    expandedTasksId.includes(subTaskId)
-  );
-  const toggleExpandAll = () => {
-    setExpandedTaskAtom((expandedTasks) => {
-      const shouldExpand = expandedTasks.includes(toggleTaskId);
-      if (shouldExpand) {
-        return Array.from(new Set([...expandedTasks, ...allTasks]));
-      }
-      return expandedTasks.filter(
-        (expandedTaskId) => !allTasks.includes(expandedTaskId)
-      );
-    });
-  };
-
-  return {
-    allExpanded,
-    toggleExpandAll,
   };
 };
